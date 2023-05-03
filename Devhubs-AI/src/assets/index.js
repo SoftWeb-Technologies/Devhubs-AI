8916aeb157c01050ddb4553e296a7d52b1cfe536
Darshanmandade <mandadedarshan@gmail.com>
2023-05-03 14:42:44 +0530

    Devhub AI

---
+++ Devhubs-AI/src/assets/index.js
@@ -0,0 +1,13 @@
+import linkIcon from "./link.svg";
+import logo from "./logo.svg";
+import loader from "./loader.svg";
+import tick from "./tick.svg";
+import copy from "./copy.svg";
+
+export {
+    linkIcon,
+    logo,
+    loader,
+    tick,
+    copy
+}
