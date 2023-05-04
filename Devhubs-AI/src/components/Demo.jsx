deaee5821c83beeb33449444e0dfb89c4cc07e1b
mg143pavankumar <pavansharma.mg143@gmail.com>
2023-05-04 13:33:29 +0530

    fixed article fetching related issues

--- Devhubs-AI/src/components/Demo.jsx
+++ Devhubs-AI/src/components/Demo.jsx
@@ -1,8 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import { copy, linkIcon, loader, tick } from "../assets";
-import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
   const [article, setArticle] = useState({
@@ -11,9 +11,8 @@ const Demo = () => {
   });
   const [allArticles, setAllArticles] = useState([]);
   const [copied, setCopied] = useState("");
-
-  // RTK lazy query
-  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState(false);
 
   // Load data from localStorage on mount
   useEffect(() => {
@@ -29,22 +28,7 @@ const Demo = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const existingArticle = allArticles.find(
-      (item) => item.url === article.url
-    );
-
-    if (existingArticle) return setArticle(existingArticle);
-
-    const { data } = await getSummary({ articleUrl: article.url });
-    if (data?.summary) {
-      const newArticle = { ...article, summary: data.summary };
-      const updatedAllArticles = [newArticle, ...allArticles];
-
-      // update state and local storage
-      setArticle(newArticle);
-      setAllArticles(updatedAllArticles);
-      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
-    }
+    fetchSummary(article.url);
   };
 
   // copy the url and toggle the icon for user feedback
@@ -60,53 +44,87 @@ const Demo = () => {
     }
   };
 
+  const fetchSummary = async (articleUrl) => {
+    try {
+      const options = {
+        method: "GET",
+        url: "https://article-extractor-and-summarizer.p.rapidapi.com/summarize",
+        params: {
+          url: `${articleUrl}`,
+          length: "3",
+        },
+        headers: {
+          "X-RapidAPI-Key":
+            "d898d63b31mshc1f9d4376966a37p1cf140jsnb42432f5eb0f",
+          "X-RapidAPI-Host": "article-extractor-and-summarizer.p.rapidapi.com",
+          "Cache-Control": "max-age=300", // cache for 5 minutes
+        },
+      };
+
+      setIsFetching(true);
+
+      const response = await axios.request(options);
+      console.log(response.status);
+
+      setIsFetching(false);
+
+      if (response.data) {
+        setArticle({ ...setArticle, summary: response.data.summary });
+      }
+    } catch (err) {
+      console.error(err.message, "some error");
+      setError(true);
+      setIsFetching(false);
+    }
+  };
+
   return (
-    <section className='mt-16 w-full max-w-xl'>
+    <section className="mt-16 w-full max-w-xl">
       {/* Search */}
-      <div className='flex flex-col w-full gap-2'>
+      <div className="flex flex-col w-full gap-2">
         <form
-          className='relative flex justify-center items-center'
+          className="relative flex justify-center items-center"
           onSubmit={handleSubmit}
         >
           <img
             src={linkIcon}
-            alt='link-icon'
-            className='absolute left-0 my-2 ml-3 w-5'
+            alt="link-icon"
+            className="absolute left-0 my-2 ml-3 w-5"
           />
 
           <input
-            type='url'
-            placeholder='Paste the article link'
+            type="url"
+            placeholder="Paste the article link"
             value={article.url}
             onChange={(e) => setArticle({ ...article, url: e.target.value })}
             onKeyDown={handleKeyDown}
             required
-            className='url_input peer' // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
+            className="url_input peer" // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
           />
           <button
-            type='submit'
-            className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 '
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 "
           >
             <p>↵</p>
           </button>
         </form>
 
         {/* Browse History */}
-        <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
           {allArticles.reverse().map((item, index) => (
             <div
               key={`link-${index}`}
               onClick={() => setArticle(item)}
-              className='link_card'
+              className="link_card"
             >
-              <div className='copy_btn' onClick={() => handleCopy(item.url)}>
+              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
                 <img
                   src={copied === item.url ? tick : copy}
                   alt={copied === item.url ? "tick_icon" : "copy_icon"}
-                  className='w-[40%] h-[40%] object-contain'
+                  className="w-[40%] h-[40%] object-contain"
                 />
               </div>
-              <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
                 {item.url}
               </p>
             </div>
@@ -115,25 +133,34 @@ const Demo = () => {
       </div>
 
       {/* Display Result */}
-      <div className='my-10 max-w-full flex justify-center items-center'>
+      <div className="my-10 max-w-full flex justify-center items-center">
         {isFetching ? (
-          <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
+          <div className="flex flex-col justify-center items-center">
+            <img
+              src={loader}
+              alt="loader"
+              className="w-20 h-20 object-contain"
+            />
+            <p className="font-satoshi text-[18px]">
+              Your request is in progress. Exciting stuff is on the way!
+            </p>
+          </div>
         ) : error ? (
-          <p className='font-inter font-bold text-black text-center'>
+          <p className="font-inter font-bold text-black text-center">
             Well, that wasn`t supposed to happen...
             <br />
-            <span className='font-satoshi font-normal text-gray-700'>
+            <span className="font-satoshi font-normal text-gray-700">
               {error?.data?.error}
             </span>
           </p>
         ) : (
           article.summary && (
-            <div className='flex flex-col gap-3'>
-              <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
-                Article <span className='blue_gradient'>Summary</span>
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
               </h2>
-              <div className='summary_box'>
-                <p className='font-inter font-medium text-sm text-gray-700'>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">
                   {article.summary}
                 </p>
               </div>
